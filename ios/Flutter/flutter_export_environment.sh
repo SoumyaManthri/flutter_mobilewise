7d33671a6cbc429dbfe361e033465627cefdcc2e
SoumyaManthri <SoumyaManthri@vassarlabs.com>
2024-04-22 11:07:49 +0530

    New Mobilewise changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/vassar-divyanshu/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/vassar-divyanshu/AndroidStudioProjects/mobilewise_sravan/flutter_mobilewise"
+export "FLUTTER_ROOT=/Users/vassarlabs/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/vassarlabs/Downloads/flutter_mobilewise"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
